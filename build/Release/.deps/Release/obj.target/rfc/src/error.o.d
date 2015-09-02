f16945e138b9a677b9f51a2ea2cd3b10350a57b4
D037732 <srdjan.boskovic@sap.com>
2015-09-02 15:54:52 +0200

    0.12 api binary

---
+++ build/Release/.deps/Release/obj.target/rfc/src/error.o.d
@@ -0,0 +1,21 @@
+cmd_Release/obj.target/rfc/src/error.o := g++ '-DNODE_GYP_MODULE_NAME=rfc' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DSAPwithUNICODE' '-DSAPwithTHREADS' '-DNDEBUG' '-DSAPonLIN' '-DSAPonUNIX' '-D__NO_MATH_INLINES' '-DBUILDING_NODE_EXTENSION' -I/home/pos/.node-gyp/0.12.7/src -I/home/pos/.node-gyp/0.12.7/deps/uv/include -I/home/pos/.node-gyp/0.12.7/deps/v8/include -I/usr/local/sap/nwrfcsdk/include  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -ffunction-sections -fdata-sections -fno-tree-vrp -fno-tree-sink -fno-omit-frame-pointer -fno-rtti -fno-exceptions -MMD -MF ./Release/.deps/Release/obj.target/rfc/src/error.o.d.raw  -c -o Release/obj.target/rfc/src/error.o ../src/error.cc
+Release/obj.target/rfc/src/error.o: ../src/error.cc ../src/error.h \
+ /home/pos/.node-gyp/0.12.7/src/node.h \
+ /home/pos/.node-gyp/0.12.7/deps/v8/include/v8.h \
+ /home/pos/.node-gyp/0.12.7/deps/v8/include/v8stdint.h \
+ /home/pos/.node-gyp/0.12.7/deps/v8/include/v8config.h \
+ /home/pos/.node-gyp/0.12.7/src/node_version.h \
+ /usr/local/sap/nwrfcsdk/include/sapnwrfc.h \
+ /usr/local/sap/nwrfcsdk/include/sapucx.h \
+ /usr/local/sap/nwrfcsdk/include/sapuc.h ../src/rfcio.h
+../src/error.cc:
+../src/error.h:
+/home/pos/.node-gyp/0.12.7/src/node.h:
+/home/pos/.node-gyp/0.12.7/deps/v8/include/v8.h:
+/home/pos/.node-gyp/0.12.7/deps/v8/include/v8stdint.h:
+/home/pos/.node-gyp/0.12.7/deps/v8/include/v8config.h:
+/home/pos/.node-gyp/0.12.7/src/node_version.h:
+/usr/local/sap/nwrfcsdk/include/sapnwrfc.h:
+/usr/local/sap/nwrfcsdk/include/sapucx.h:
+/usr/local/sap/nwrfcsdk/include/sapuc.h:
+../src/rfcio.h:
---
+++ build/Release/.deps/Release/rfc.node.d
@@ -0,0 +1,1 @@
+cmd_Release/rfc.node := rm -rf "Release/rfc.node" && cp -af "linux_x64/rfc.node" "Release/rfc.node"
