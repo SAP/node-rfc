310523fbe68a7941494171988c0093e710f43087
bsrdjan <srdjan.boskovic@sap.com>
2018-04-03 13:12:04 +0200

    [publish binary] version to 0.1.14

--- release.sh
+++ release.sh
@@ -1,57 +1,55 @@
 # release build script
+#
+# Prerequisites:
+# 1. node-rfc-release, at the same level as node-rfc
+# 2. node-pre-gyp must be installed globally on windows
+# 3. Pythn 2.7, required by node-gyp
+#
+# Run:
+# source release.sh
 
 release_output="../../node-rfc-release"
+release_root="../node-rfc-release"
+version=`cat ./VERSION` 
+
+declare -a LTS_VERSIONS=("4.9.1" "6.13.1" "8.10.0")
 
 if [ "$(expr substr $(uname -s) 1 4)" == "MSYS" ]; then
 
-    rm $release_output/*win32-x64.tar.gz
+    rm $release_root/*win32-x64.tar.gz
 
-    nvm use 4.8.6
-    node-pre-gyp clean configure
-    node-pre-gyp build
-    cd build
-    tar -czvf $release_output/rfc-v0.1.13-node-v46-win32-x64.tar.gz rfc
-    cd ..
-
-    nvm use 6.12.0
-    node-pre-gyp clean configure
-    node-pre-gyp build
-    cd build
-    tar -czvf $release_output/rfc-v0.1.13-node-v48-win32-x64.tar.gz rfc
-    cd ..
-
-    nvm use 8.9.1
-    node-pre-gyp clean configure
-    node-pre-gyp build
-    cd build
-    tar -czvf $release_output/rfc-v0.1.13-node-v57-win32-x64.tar.gz rfc
-    cd ..
+    for lts in "${LTS_VERSIONS[@]}"
+    do
+        nvm use $lts
+        rm -rf node_modules
+        npm install
+        abi=`node --eval "console.log(require('node-abi').getAbi())"`
+        echo "nodejs $lts abi $abi ====================================="
+        npm uninstall node-pre-gyp # issues with local node-pre-gyp on windows
+        npm -g install node-pre-gyp
+        node-pre-gyp clean configure
+        node-pre-gyp build
+        cd build
+        tar -czvf $release_output/rfc-v${version}-node-v${abi}-win32-x64.tar.gz rfc
+        cd ..
+    done
 
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
 
-    rm $release_output/*linux-x64.tar.gz
+    rm $release_root/*linux-x64.tar.gz
 
-    export NVM_DIR="$HOME/.nvm"
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+    for lts in "${LTS_VERSIONS[@]}"
+    do
+        nvm use $lts
+        rm -rf node_modules
+        npm install
+        abi=`node --eval "console.log(require('node-abi').getAbi())"`
+        echo "nodejs $lts abi $abi ====================================="
+        node-pre-gyp clean configure
+        node-pre-gyp build
+        cd build
+        tar -czvf $release_output/rfc-v${version}-node-v${abi}-linux-x64.tar.gz rfc
+        cd ..
+    done
 
-    nvm use 4
-    node-pre-gyp clean configure
-    node-pre-gyp build
-    cd build
-    tar -czvf $release_output/rfc-v0.1.13-node-v46-linux-x64.tar.gz rfc
-    cd ..
-
-    nvm use 6
-    node-pre-gyp clean configure
-    node-pre-gyp build
-    cd build
-    tar -czvf $release_output/rfc-v0.1.13-node-v48-linux-x64.tar.gz rfc
-    cd ..
-
-    nvm use 8
-    node-pre-gyp clean configure
-    node-pre-gyp build
-    cd build
-    tar -czvf $release_output/rfc-v0.1.13-node-v57-linux-x64.tar.gz rfc
-    cd ..
 fi
