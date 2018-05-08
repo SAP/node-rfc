1c0ab38637bc63760ac6d98e7d5790131fd69320
bsrdjan <srdjan.boskovic@sap.com>
2018-05-08 08:17:17 +0000

    string handling, c/c++ style guide

--- src/rfcio.h
+++ src/rfcio.h
@@ -19,18 +19,15 @@
 #include <node.h>
 #include <sapnwrfc.h>
 
-
 using namespace v8;
 
-
-std::string convertToString(v8::Handle<v8::Value> const &str);
-SAP_UC* fillString(const Handle<Value> &str);
+SAP_UC *fillString(const Handle<Value> &str);
 Handle<Value> fillFunctionParameter(RFC_FUNCTION_DESC_HANDLE functionDescHandle, RFC_FUNCTION_HANDLE functionHandle, Local<Value> name, Local<Value> value);
-Local<Value> fillVariable(RFCTYPE type, RFC_FUNCTION_HANDLE functionHandle, SAP_UC* cName, Handle<Value> value, RFC_TYPE_DESC_HANDLE functionDescHandle);
+Local<Value> fillVariable(RFCTYPE type, RFC_FUNCTION_HANDLE functionHandle, SAP_UC *cName, Handle<Value> value, RFC_TYPE_DESC_HANDLE functionDescHandle);
 
-Handle<Object> wrapResult(RFC_FUNCTION_DESC_HANDLE functionDescHandle, RFC_FUNCTION_HANDLE functionHandle, bool rstrip=false);
-Handle<Value> wrapStructure(RFC_TYPE_DESC_HANDLE typeDesc, RFC_STRUCTURE_HANDLE structHandle, bool rstrip=false);
-Handle<Value> wrapString(SAP_UC* uc, int length=-1, bool rstrip=false);
-Handle<Value> wrapVariable(RFCTYPE type, RFC_FUNCTION_HANDLE container, SAP_UC* cName, unsigned int cLen, RFC_TYPE_DESC_HANDLE typeDesc, bool rstrip=false);
+Handle<Object> wrapResult(RFC_FUNCTION_DESC_HANDLE functionDescHandle, RFC_FUNCTION_HANDLE functionHandle, bool rstrip = false);
+Handle<Value> wrapStructure(RFC_TYPE_DESC_HANDLE typeDesc, RFC_STRUCTURE_HANDLE structHandle, bool rstrip = false);
+Handle<Value> wrapString(SAP_UC *uc, int length = -1, bool rstrip = false);
+Handle<Value> wrapVariable(RFCTYPE type, RFC_FUNCTION_HANDLE container, SAP_UC *cName, unsigned int cLen, RFC_TYPE_DESC_HANDLE typeDesc, bool rstrip = false);
 
 #endif
