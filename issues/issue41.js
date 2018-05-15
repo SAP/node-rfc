c913f5e68b9ae399c0e8aff55be54dc8fa56ca17
bsrdjan <srdjan.boskovic@sap.com>
2018-05-15 11:15:43 +0000

    connection parameters

--- issues/issue41.js
+++ issues/issue41.js
@@ -1,52 +1,62 @@
-let rfc = require("../build/linux_x64/rfc.node");
+let rfc = require('../build/linux_x64/rfc.node');
+const connParams = require('../test/connParams');
 
-let connParams = {
-  user: 'demo',
-  passwd: 'Welcome',
-  ashost:  '10.117.19.101',
-  saprouter: '/H/203.13.155.17/W/xjkb3d/H/172.19.138.120/H/',
-  sysnr: '00',
-  lang: 'EN',
-  client: '100'
-}
-
-let fs = require ("fs");
+let fs = require('fs');
 let fileName = `xxx-withClose-${process.version}.txt`;
-fs.writeFile(fileName, `iteration\t\trss\t\theapTotal\t\theapUsed\t\texternal\n`)
+fs.writeFile(fileName, `iteration\t\trss\t\theapTotal\t\theapUsed\t\texternal\n`);
 
 let connections = [];
 
-function run (i) {
+function run(i) {
+	console.log(i);
 
-    console.log(i);
+	if (i == 500) {
+		console.log(connections.length, connections.every(v => v.isAlive() == false));
+		return;
+	}
 
-    if (i == 500) {
-      console.log(connections.length, connections.every(v => v.isAlive() == false));
-      return;
-    }
+	let client = new rfc.Client(connParams);
+	connections[i] = client;
 
-    let client = new rfc.Client(connParams);
-    connections[i] = client;
-
-    client.connect(function (err) {
-            if (err) { // check for login/connection errors
-                        return console.error('could not connect to server', err);
-                    }
-            client.invoke('SWNC_READ_SNAPSHOT', {READ_TIMEZONE: 'UTC', READ_START_DATE: '20180411', READ_START_TIME: '080000', READ_END_DATE: '20180418', READ_END_TIME: '000000', TIME_RESOLUTION: 60}, function (err, res) {
-            //client.invoke("STFC_PERFORMANCE", {"CHECKTAB":"X","LGET0332":"999", "LGET1000": "999"}, function (err, res) {
-              if (err) {
-                console.log(err)
-                fs.appendFileSync(fileName, err);
-              } else {
-                        let memUsage = process.memoryUsage();
-                        let line = `${i}\t\t${(memUsage.rss / 1024 / 1024).toFixed(3)}MB\t\t${(memUsage.heapTotal / 1024 / 1024).toFixed(3)}MB\t\t${(memUsage.heapUsed / 1024 / 1024).toFixed(3)}MB\t\t${(memUsage.external / 1024 / 1024).toFixed(3)}MB\t\t${err ? err : ""}\n`;
-                        connections[i].close();
-                        fs.appendFile(fileName, line);
-                        run(++i);
-                        delete res;
-              }
-            });
-        });
+	client.connect(function(err) {
+		if (err) {
+			// check for login/connection errors
+			return console.error('could not connect to server', err);
+		}
+		client.invoke(
+			'SWNC_READ_SNAPSHOT',
+			{
+				READ_TIMEZONE: 'UTC',
+				READ_START_DATE: '20180411',
+				READ_START_TIME: '080000',
+				READ_END_DATE: '20180418',
+				READ_END_TIME: '000000',
+				TIME_RESOLUTION: 60,
+			},
+			function(err, res) {
+				//client.invoke("STFC_PERFORMANCE", {"CHECKTAB":"X","LGET0332":"999", "LGET1000": "999"}, function (err, res) {
+				if (err) {
+					console.log(err);
+					fs.appendFileSync(fileName, err);
+				} else {
+					let memUsage = process.memoryUsage();
+					let line = `${i}\t\t${(memUsage.rss / 1024 / 1024).toFixed(3)}MB\t\t${(
+						memUsage.heapTotal /
+						1024 /
+						1024
+					).toFixed(3)}MB\t\t${(memUsage.heapUsed / 1024 / 1024).toFixed(3)}MB\t\t${(
+						memUsage.external /
+						1024 /
+						1024
+					).toFixed(3)}MB\t\t${err ? err : ''}\n`;
+					connections[i].close();
+					fs.appendFile(fileName, line);
+					run(++i);
+					delete res;
+				}
+			}
+		);
+	});
 }
 
 run(1);
--- issues/issue50.js
+++ issues/issue50.js
@@ -1,14 +1,5 @@
-let rfc = require("../build/linux_x64/rfc.node");
-
-let connParams = {
-  user: 'demo',
-  passwd: 'Welcome',
-  ashost: '10.117.19.101',
-  saprouter: '/H/203.13.155.17/W/xjkb3d/H/172.19.138.120/H/',
-  sysnr: '00',
-  lang: 'EN',
-  client: '100'
-}
+let rfc = require('../build/linux_x64/rfc.node');
+const connParams = require('../test/connParams');
 
 client = new rfc.Client(connParams);
 /*
@@ -40,28 +31,25 @@ return;
 */
 
 let importStruct = {
-  //RFCINT1: 1,
-  //RFCINT2: 2,
-  //RFCINT4: 3,
-  //RFCCHAR4: 65,
-  RFCFLOAT: "A"
-}
+	//RFCINT1: 1,
+	//RFCINT2: 2,
+	//RFCINT4: 3,
+	//RFCCHAR4: 65,
+	RFCFLOAT: 'A',
+};
 
-let importTable = [importStruct]
+let importTable = [importStruct];
 
-
-client.connect(function (err) {
-  if (err) {
-    return console.error('could not connect to server', err);
-  }
-  client.invoke('STFC_STRUCTURE',
-    { IMPORTSTRUCT: importStruct, RFCTABLE: importTable },
-    function (err, res) {
-      if (err) {
-        return console.error(err);
-      }
-      console.log(importStruct, { RFCFLOAT: res.ECHOSTRUCT.RFCFLOAT } );
-      //console.log(importStruct, { RFCCHAR4: res.ECHOSTRUCT.RFCCHAR4 } );
-      //console.log(importStruct, { RFCINT1: res.ECHOSTRUCT.RFCINT1, RFCINT2: res.ECHOSTRUCT.RFCINT2, RFCINT4: res.ECHOSTRUCT.RFCINT4});
-    });
+client.connect(function(err) {
+	if (err) {
+		return console.error('could not connect to server', err);
+	}
+	client.invoke('STFC_STRUCTURE', { IMPORTSTRUCT: importStruct, RFCTABLE: importTable }, function(err, res) {
+		if (err) {
+			return console.error(err);
+		}
+		console.log(importStruct, { RFCFLOAT: res.ECHOSTRUCT.RFCFLOAT });
+		//console.log(importStruct, { RFCCHAR4: res.ECHOSTRUCT.RFCCHAR4 } );
+		//console.log(importStruct, { RFCINT1: res.ECHOSTRUCT.RFCINT1, RFCINT2: res.ECHOSTRUCT.RFCINT2, RFCINT4: res.ECHOSTRUCT.RFCINT4});
+	});
 });
