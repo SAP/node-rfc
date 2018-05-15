7fec5555175880e77949fd965f35ae6cbab4d4de
bsrdjan <srdjan.boskovic@sap.com>
2018-05-15 11:15:43 +0000

    connection parameters

--- test/test.js
+++ test/test.js
@@ -20,25 +20,7 @@ const rfc_path = binary.find(path.resolve(path.join(__dirname, '../package.json'
 const rfc = require(rfc_path);
 const should = require('should');
 const Decimal = require('decimal.js');
-
-let connParams = {
-	user: 'demo',
-	passwd: 'Welcome',
-	ashost: '10.117.19.101',
-	saprouter: '/H/203.13.155.17/W/xjkb3d/H/172.19.138.120/H/',
-	sysnr: '00',
-	lang: 'EN',
-	client: '100',
-};
-
-connParams = {
-	user: 'demo',
-	passwd: 'welcome',
-	ashost: '10.68.110.51',
-	sysnr: '00',
-	client: '620',
-	lang: 'EN',
-};
+const connParams = require('./connParams');
 
 describe('Connection', function() {
 	let client;
