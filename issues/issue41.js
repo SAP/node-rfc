f27db8965d45dae4a9d9e675b58e44087ff5e5a8
bsrdjan <srdjan.boskovic@sap.com>
2018-05-16 10:45:03 +0000

    memleak test

--- issues/issue41.js
+++ issues/issue41.js
@@ -1,61 +1,65 @@
 let rfc = require('../build/linux_x64/rfc.node');
 const connParams = require('../test/connParams');
+const branchName = require('current-git-branch')();
 
 let fs = require('fs');
-let fileName = `xxx-withClose-${process.version}.txt`;
-fs.writeFile(fileName, `iteration\t\trss\t\theapTotal\t\theapUsed\t\texternal\n`);
 
-let connections = [];
+const COUNT = 200;
+const SIZE = '10000';
+
+let fileName = `exp/${branchName}-${COUNT}-x-${SIZE}-${process.version}.txt`;
+fs.writeFileSync(fileName, `iteration\t\trss\t\theapTotal\t\theapUsed\t\texternal\n`);
 
 function run(i) {
 	console.log(i);
 
-	if (i == 500) {
-		console.log(connections.length, connections.every(v => v.isAlive() == false));
+	if (i == COUNT) {
+		console.log('Bye!');
 		return;
 	}
 
 	let client = new rfc.Client(connParams);
-	connections[i] = client;
 
 	client.connect(function(err) {
 		if (err) {
 			// check for login/connection errors
 			return console.error('could not connect to server', err);
 		}
-		client.invoke(
-			'SWNC_READ_SNAPSHOT',
-			{
-				READ_TIMEZONE: 'UTC',
-				READ_START_DATE: '20180411',
-				READ_START_TIME: '080000',
-				READ_END_DATE: '20180418',
-				READ_END_TIME: '000000',
-				TIME_RESOLUTION: 60,
-			},
-			function(err, res) {
-				//client.invoke("STFC_PERFORMANCE", {"CHECKTAB":"X","LGET0332":"999", "LGET1000": "999"}, function (err, res) {
-				if (err) {
-					console.log(err);
-					fs.appendFileSync(fileName, err);
-				} else {
-					let memUsage = process.memoryUsage();
-					let line = `${i}\t\t${(memUsage.rss / 1024 / 1024).toFixed(3)}MB\t\t${(
-						memUsage.heapTotal /
-						1024 /
-						1024
-					).toFixed(3)}MB\t\t${(memUsage.heapUsed / 1024 / 1024).toFixed(3)}MB\t\t${(
-						memUsage.external /
-						1024 /
-						1024
-					).toFixed(3)}MB\t\t${err ? err : ''}\n`;
-					connections[i].close();
-					fs.appendFile(fileName, line);
-					run(++i);
-					delete res;
-				}
+		/*
+        client.invoke(
+            'SWNC_READ_SNAPSHOT',
+            {
+                READ_TIMEZONE: 'UTC',
+                READ_START_DATE: '20180411',
+                READ_START_TIME: '080000',
+                READ_END_DATE: '20180418',
+                READ_END_TIME: '000000',
+                TIME_RESOLUTION: 60,
+            },
+            function(err, res) {
+        */
+
+		client.invoke('STFC_PERFORMANCE', { CHECKTAB: 'X', LGET0332: SIZE, LGET1000: SIZE }, function(err, res) {
+			if (err) {
+				console.log(err);
+				fs.appendFileSync(fileName, err);
+			} else {
+				let memUsage = process.memoryUsage();
+				let line = `${i}\t\t${(memUsage.rss / 1024 / 1024).toFixed(3)}MB\t\t${(
+					memUsage.heapTotal /
+					1024 /
+					1024
+				).toFixed(3)}MB\t\t${(memUsage.heapUsed / 1024 / 1024).toFixed(3)}MB\t\t${(
+					memUsage.external /
+					1024 /
+					1024
+				).toFixed(3)}MB\t\t${err ? err : ''}\n`;
+				client.close();
+				delete client;
+				fs.appendFileSync(fileName, line);
+				run(++i);
 			}
-		);
+		});
 	});
 }
 
