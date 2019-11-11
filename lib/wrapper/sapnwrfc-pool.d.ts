ddd8073c70f8c61b8b2c68913d1c3922d9f686f7
bsrdjan <srdjan.boskovic@sap.com>
2019-11-11 15:51:07 +0100

    release 1.0.2

--- lib/wrapper/sapnwrfc-pool.d.ts
+++ lib/wrapper/sapnwrfc-pool.d.ts
@@ -1,5 +1,5 @@
-import * as Promise from 'bluebird';
-import { Client, RfcConnectionParameters } from './sapnwrfc-client';
+import * as Promise from "bluebird";
+import { Client, RfcConnectionParameters, RfcClientOptions } from "./sapnwrfc-client";
 export interface RfcPoolOptions {
     min: number;
     max: number;
@@ -7,10 +7,11 @@ export interface RfcPoolOptions {
 export declare class Pool {
     private __connectionParams;
     private __poolOptions;
+    private __clientOptions;
     private __clients;
-    constructor(connectionParams: RfcConnectionParameters, poolOptions?: RfcPoolOptions);
+    constructor(connectionParams: RfcConnectionParameters, poolOptions?: RfcPoolOptions, clientOptions?: RfcClientOptions);
     acquire(): Promise<Client | Error> | undefined;
     release(client: Client): void;
-    releaseAll(): void;
+    releaseAll(): Promise<void>;
     readonly status: object;
 }
