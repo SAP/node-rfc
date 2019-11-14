86601c973e55c454e4e02562a02ec451cc989602
bsrdjan <srdjan.boskovic@sap.com>
2019-11-14 09:42:51 +0100

    Windows build options cleanup

--- src/node_sapnwrfc.cc
+++ src/node_sapnwrfc.cc
@@ -12,7 +12,7 @@
 // either express or implied. See the License for the specific
 // language governing permissions and limitations under the License.
 
-#include "client.h"
+#include "Client.h"
 #include "macros.h"
 
 using namespace node_rfc;
