65d60112bc2e44360775b4c75dd0a7d9467cbd76
bsrdjan <srdjan.boskovic@sap.com>
2020-04-06 20:03:43 +0200

    Pool w. JS mutex

--- lib/wrapper/sapnwrfc-pool.d.ts
+++ lib/wrapper/sapnwrfc-pool.d.ts
@@ -1,4 +1,3 @@
-import * as Promise from "bluebird";
 import { Client, RfcConnectionParameters, RfcClientOptions } from "./sapnwrfc-client";
 export interface RfcPoolOptions {
     min: number;
@@ -8,10 +7,11 @@ export declare class Pool {
     private __connectionParams;
     private __poolOptions;
     private __clientOptions;
+    private __mutex;
     private __clients;
     constructor(connectionParams: RfcConnectionParameters, poolOptions?: RfcPoolOptions, clientOptions?: RfcClientOptions);
-    acquire(): Promise<Client | Error> | undefined;
-    release(client: Client): void;
+    acquire(): Promise<Client | TypeError | void>;
+    release(client: Client): Promise<void>;
     releaseAll(): Promise<void>;
     get status(): object;
 }
--- src/ts/wrapper/sapnwrfc-pool.ts
+++ src/ts/wrapper/sapnwrfc-pool.ts
@@ -1,89 +1,121 @@
-import * as Promise from "bluebird";
+//import * as Promise from "bluebird";
 import {
     Client,
     RfcConnectionParameters,
-    RfcClientOptions
+    RfcClientOptions,
 } from "./sapnwrfc-client";
+import { isUndefined } from "util";
 
 export interface RfcPoolOptions {
     min: number;
     max: number;
+}
+class Mutex {
+    private _lock: Promise<any>;
+
+    constructor() {
+        this._lock = Promise.resolve();
+    }
+
+    _acquire() {
+        let release;
+        const lock = (this._lock = new Promise((resolve) => {
+            release = resolve;
+        }));
+        return release;
+    }
+    acquire(): Promise<any> {
+        const q = this._lock.then(() => release);
+        const release = this._acquire();
+        return q;
+    }
 }
 
 export class Pool {
     private __connectionParams: RfcConnectionParameters;
     private __poolOptions: RfcPoolOptions;
     private __clientOptions: RfcClientOptions | undefined;
+    private __mutex: Mutex;
 
-    private __clients: { [key: string]: Map<number, Promise<Client>> };
+    private __clients: {
+        ready: Map<number, Promise<Client>>;
+        active: Set<number>;
+    };
 
     constructor(
         connectionParams: RfcConnectionParameters,
         poolOptions: RfcPoolOptions = {
             min: 2,
-            max: 50
+            max: 50,
         },
         clientOptions?: RfcClientOptions
     ) {
+        this.__mutex = new Mutex();
         this.__connectionParams = connectionParams;
         this.__poolOptions = poolOptions;
         this.__clientOptions = clientOptions;
         this.__clients = {
             ready: new Map(),
-            active: new Map()
+            active: new Set(),
         };
     }
 
-    acquire(): Promise<Client | Error> | undefined {
-        for (
-            let i = this.__clients.ready.size;
-            i < this.__poolOptions.min;
-            i++
-        ) {
-            let client = this.__clientOptions
-                ? new Client(this.__connectionParams, this.__clientOptions)
-                : new Client(this.__connectionParams);
-            this.__clients.ready.set(client.id, client.open());
-        }
-        if (this.__clients.ready.size === 0) {
-            return new Promise((resolve, reject) =>
-                reject(TypeError("Internal pool error, size = 0"))
-            );
-        }
-        let id = this.__clients.ready.keys().next().value;
-        let client = this.__clients.ready.get(id);
-        this.__clients.ready.delete(id);
-        return client;
+    acquire(): Promise<Client | TypeError | void> {
+        return (async () => {
+            const unlock = await this.__mutex.acquire();
+
+            for (
+                let i = this.__clients.ready.size;
+                i < this.__poolOptions.min;
+                i++
+            ) {
+                let client = this.__clientOptions
+                    ? new Client(this.__connectionParams, this.__clientOptions)
+                    : new Client(this.__connectionParams);
+                this.__clients.ready.set(client.id, client.open());
+            }
+            if (this.__clients.ready.size === 0) {
+                return new Promise<TypeError>((resolve, reject) =>
+                    reject(TypeError("Internal pool error, size = 0"))
+                );
+            }
+            let id = this.__clients.ready.keys().next().value;
+            let client = this.__clients.ready.get(id);
+            this.__clients.ready.delete(id);
+            this.__clients.active.add(id);
+            unlock();
+            return client;
+        })();
     }
 
     release(client: Client) {
-        client.close().then(() => {});
-        //client.close(() => {
-        //    this.__clients.ready.set(client.id, client.open());
-        //});
+        return (async () => {
+            await client.close();
+            const unlock = await this.__mutex.acquire();
+            if (this.__clients.ready.size < this.__poolOptions.min) {
+                this.__clients.ready.set(client.id, client.open());
+                this.__clients.active.delete(client.id);
+            }
+            unlock();
+        })();
     }
 
     releaseAll(): Promise<void> {
-        let promises: Array<Promise<Client>> = [];
-        this.__clients.ready.forEach(cp => {
-            cp.then(client => {
-                promises.push(
-                    client.close().then(() => {
-                        //this.__clients.ready.delete(client.id);
-                    })
-                );
-            });
-        });
-        return Promise.all(promises).then(() => {
-            this.__clients.ready = new Map();
-        });
+        return (async () => {
+            const unlock = await this.__mutex.acquire();
+            for (const [id, client] of this.__clients.ready) {
+                this.__clients.ready.delete(id);
+                client.then((c) => c.close());
+            }
+            unlock();
+        })();
     }
 
     get status(): object {
         return {
-            //active: this.__clients.active.size,
+            active: this.__clients.active.size,
             ready: this.__clients.ready.size,
-            options: this.__poolOptions
+            options: this.__poolOptions,
         };
     }
 }
