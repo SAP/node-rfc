4f2cc3cbe5f535a306428ba62f83038acb728d8d
bsrdjan <srdjan.boskovic@sap.com>
2020-07-27 14:07:41 +0200

    Accidentally added p-queue dependency removed

--- src/noderfc.h
+++ src/noderfc.h
@@ -35,7 +35,7 @@
 #define NODERFC_VERSION "Deactivate logging: LOG_RFC_CLIENT"
 #else
 // client binding version
-#define NODERFC_VERSION "2.0.2"
+#define NODERFC_VERSION "2.0.3"
 #endif
 
 template <typename... Args>
