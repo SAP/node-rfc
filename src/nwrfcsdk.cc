02f64fe744395c327d4db8b75255c75ea5dda4bd
Samuel Allan <naclo3samuel@gmail.com>
2021-12-24 07:46:35 -0500

    Fixed memory leak for RFCTYPE_XSTRING
    nwrfcsdk.cc:618 - byteValue does not free automatically, added finalizer to free byteValue once no longer needed.

--- src/nwrfcsdk.cc
+++ src/nwrfcsdk.cc
@@ -615,8 +615,9 @@ namespace node_rfc
                 free(byteValue);
                 break;
             }
-            resultValue = Napi::Buffer<SAP_RAW>::New(node_rfc::__env, reinterpret_cast<SAP_RAW *>(byteValue), resultLen);
-            // do not free byteValue - it will be freed when the buffer is garbage collected
+            resultValue = Napi::Buffer<SAP_RAW>::New(node_rfc::__env, reinterpret_cast<SAP_RAW *>(byteValue), resultLen, [](Env env, SAP_RAW *garbage) {
+                free(garbage);
+            });
             break;
         }
         case RFCTYPE_BCD:
