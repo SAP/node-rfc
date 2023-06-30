49f8974882afa96697dafb24bc66db0896ccc3d3
bsrdjan <srdjan.boskovic@sap.com>
2023-06-30 10:58:28 +0200

    refactor(cpp): 3.0.0-rc.1

--- src/cpp/noderfc.h
+++ src/cpp/noderfc.h
@@ -25,7 +25,7 @@
 #define NODERFC_VERSION "Deactivate logging: LOG_RFC_CLIENT"
 #else
 // client binding version
-#define NODERFC_VERSION "2.7.2-rc.6"
+#define NODERFC_VERSION "3.0.0-rc.1"
 #endif
 
 template <typename... Args>
